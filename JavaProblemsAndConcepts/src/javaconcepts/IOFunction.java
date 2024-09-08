3a9f0f242a97049c12ca43d6924d0414cb827c73
NehaMN <nehamn207@gmail.com>
2024-09-08 19:53:30 +0530

    java problems and concepts

--- JavaProblemsAndConcepts/src/javaconcepts/IOFunction.java
+++ JavaProblemsAndConcepts/src/javaconcepts/IOFunction.java
@@ -1,15 +1,18 @@
 package javaconcepts;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class IOFunction {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         IOFunction1();
     }
-    static void IOFunction1() {
+    static void IOFunction1() throws  IOException {
+        System.out.println("Taking input using scanner:");
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-
         while (t-- > 0) {
             int a =sc.nextInt();
             float f= sc.nextFloat();
@@ -23,5 +26,18 @@ public class IOFunction {
             System.out.println(b);
             System.out.println(s);
         }
+
+        System.out.println("Taking input using BufferedReader:");
+        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        float f= Float.parseFloat(br.readLine());
+        long l = Long.parseLong(br.readLine());
+        byte b = Byte.parseByte(br.readLine());
+        String s= br.readLine();
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(l);
+        System.out.println(b);
+        System.out.println(s);
     }
 }
