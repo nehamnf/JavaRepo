0155a9380c64729b6e12cb8fb5a9734681971839
NehaMN <nehamn207@gmail.com>
2024-09-10 19:18:25 +0530

    java problems

---
+++ JavaProblemsAndConcepts/src/problems/LcmOfNumber.java
@@ -0,0 +1,18 @@
+package problems;
+
+public class LcmOfNumber {
+    public static void main(String[] args) {
+        System.out.println(LCM(6,9));
+
+    }
+    public static int LCM(int a, int b){
+        return ((a * b)/gcdEuclideanBetter(a,b));
+    }
+
+    public static int gcdEuclideanBetter(int num1,int num2){
+        if ( num2 == 0 ){
+            return num1;
+        }
+        return gcdEuclideanBetter(num2 , num1 % num2);
+    }
+}
---
+++ JavaProblemsAndConcepts/src/problems/PyramidPattern.java
@@ -0,0 +1,14 @@
+package problems;
+
+public class PyramidPattern {
+    public static void main(String[] args) {
+        printPyramid(4);
+    }
+
+    public static void printPyramid(int n){
+        for (int i=1;i <=n;i++){
+                    System.out.print(" ".repeat(n-i)+"* ".repeat(((2*i)-1)));
+            }
+            System.out.println();
+        }
+}
